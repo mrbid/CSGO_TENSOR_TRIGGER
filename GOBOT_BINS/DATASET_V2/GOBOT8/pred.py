c6b7b6f1b64aaf8e102ee663f3b85699339792d0
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2021-10-06 11:25:03 +0100

    Update pred.py

--- GOBOT_BINS/DATASET_V2/GOBOT8/pred.py
+++ GOBOT_BINS/DATASET_V2/GOBOT8/pred.py
@@ -11,36 +11,20 @@ from os.path import getsize
 from struct import pack
 from time import sleep
 
-# filter_resolution = 2
-# model = Sequential([
-#         keras.Input(shape=(28, 28, 3)),
-#         layers.Conv2D(filter_resolution, kernel_size=(3, 3), activation="relu"),
-#         layers.MaxPooling2D(pool_size=(2, 2)),
-#         layers.Conv2D(filter_resolution*2, kernel_size=(3, 3), activation="relu"),
-#         layers.MaxPooling2D(pool_size=(2, 2)),
-#         layers.Conv2D(filter_resolution*4, kernel_size=(3, 3), activation="relu"),
-#         layers.GlobalAveragePooling2D(),
-#         layers.Flatten(),
-#         layers.Dense(1, activation="sigmoid"),
-# ])
-# model.compile(optimizer='adam', loss='mean_squared_error')
-
-# if isfile("weights.h5"):
-#         model.load_weights("weights.h5")
-#         print("weights loaded")
-
 input_size = 2352
 
 model = keras.models.load_model("keras_model")
 input_size_floats = input_size*4
 while True:
-        sleep(0.001)
-        if isfile("input.dat") and getsize("input.dat") == input_size_floats:
-                with open("input.dat", 'rb') as f:
-                        data = np.fromfile(f, dtype=np.float32)
-                        if data.size == input_size:
-                                input = np.reshape(data, [-1, 28,28,3])
-                                r = model.predict(input)
-                                with open("r.dat", "wb") as f2:
-                                        f2.write(pack('f', r))
-
+        try:
+                sleep(0.001)
+                if isfile("input.dat") and getsize("input.dat") == input_size_floats:
+                        with open("input.dat", 'rb') as f:
+                                data = np.fromfile(f, dtype=np.float32)
+                                if data.size == input_size:
+                                        input = np.reshape(data, [-1, 28,28,3])
+                                        r = model.predict(input)
+                                        with open("r.dat", "wb") as f2:
+                                                f2.write(pack('f', r))
+        except Exception:
+                pass
