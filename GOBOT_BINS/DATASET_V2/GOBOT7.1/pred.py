11e9c5a1bf391e20060d513d364ddabfb0a362fb
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2021-10-06 11:24:37 +0100

    Update pred.py

--- GOBOT_BINS/DATASET_V2/GOBOT7.1/pred.py
+++ GOBOT_BINS/DATASET_V2/GOBOT7.1/pred.py
@@ -14,14 +14,16 @@ input_size = 2352
 model = keras.models.load_model("keras_model")
 input_size_floats = input_size*4
 while True:
-        sleep(0.001)
-        if isfile("input.dat") and getsize("input.dat") == input_size_floats:
-                with open("input.dat", 'rb') as f:
-                        data = np.fromfile(f, dtype=np.float32)
-                        remove("input.dat")
-                        if data.size == input_size:
-                                input = np.reshape(data, [-1, input_size])
-                                r = model.predict(input)
-                                with open("r.dat", "wb") as f2:
-                                        f2.write(pack('f', r))
-
+        try:
+                sleep(0.001)
+                if isfile("input.dat") and getsize("input.dat") == input_size_floats:
+                        with open("input.dat", 'rb') as f:
+                                data = np.fromfile(f, dtype=np.float32)
+                                remove("input.dat")
+                                if data.size == input_size:
+                                        input = np.reshape(data, [-1, input_size])
+                                        r = model.predict(input)
+                                        with open("r.dat", "wb") as f2:
+                                                f2.write(pack('f', r))
+        except Exception:
+                pass
