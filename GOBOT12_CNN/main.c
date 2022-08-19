a315998592131d44e81c00f8a2978ed1e26edcad
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2022-08-19 19:55:50 +0100

    Update main.c

--- GOBOT12_CNN/main.c
+++ GOBOT12_CNN/main.c
@@ -1,6 +1,6 @@
 /*
 --------------------------------------------------
-    James William Fletcher (james@voxdsp.com)
+    James William Fletcher (github.com/mrbid)
 --------------------------------------------------
 
     CS:GO
