46ef50eeb6736bec031ae39774949ff3b069d1fe
mrbid <78346668+mrbid@users.noreply.github.com>
2023-01-03 15:28:50 +0000

    general

--- GOBOT15/KERAS2C/CNN/compile.sh
+++ GOBOT15/KERAS2C/CNN/compile.sh
@@ -1,4 +1,4 @@
-clang aim.c processModel.c ../include/k2c_pooling_layers.c ../include/k2c_convolution_layers.c ../include/k2c_helper_functions.c ../include/k2c_core_layers.c ../include/k2c_activations.c -I ../include -Ofast -mfma -march=native -lX11 -dn -lm -o aim
+clang aim.c processModel.c ../include/k2c_pooling_layers.c ../include/k2c_convolution_layers.c ../include/k2c_helper_functions.c ../include/k2c_core_layers.c ../include/k2c_activations.c -I ../include -Ofast -mfma -lX11 -dn -lm -o aim
 strip --strip-unneeded aim
 upx --lzma --best aim
 ./aim
