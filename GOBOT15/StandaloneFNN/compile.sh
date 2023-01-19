11d0411642204a1642c913d7bff398fb807601eb
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2023-01-19 04:56:59 +0000

    Update compile.sh

--- GOBOT15/StandaloneFNN/compile.sh
+++ GOBOT15/StandaloneFNN/compile.sh
@@ -1,7 +1,10 @@
-clang aim1.c -Ofast -mfma -lX11 -dn -lm -o aim1
+clang aim1.c -Ofast -mfma -lX11 -lm -o aim1
 strip --strip-unneeded aim1
 upx --lzma --best aim1
-clang aim2.c -Ofast -mfma -lX11 -dn -lm -o aim2
+clang aim2.c -Ofast -mfma -lX11 -lm -o aim2
 strip --strip-unneeded aim2
 upx --lzma --best aim2
+clang aim3.c -Ofast -mfma -lX11 -lm -o aim3
+strip --strip-unneeded aim3
+upx --lzma --best aim3
 ./aim1
